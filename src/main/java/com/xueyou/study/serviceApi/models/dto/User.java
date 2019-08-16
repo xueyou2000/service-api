f79ef64ab82107b619d74fa9becd3548ca228359
XueYou <xueyoucd@gmail.com>
2019-08-16 17:09:27 +0800

    不使用父包依赖

--- src/main/java/com/xueyou/study/serviceApi/models/dto/User.java
+++ src/main/java/com/xueyou/study/serviceApi/models/dto/User.java
@@ -1,14 +1,25 @@
 package com.xueyou.study.serviceApi.models.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
  * 创建 by xueyo on 2019/8/14
  */
+@ApiModel(value = "User", description = "用户信息")
 @Data
 public class User {
 
+    /**
+     * 用户名称
+     */
+    @ApiModelProperty(value = "用户名称")
     private String name;
+    /**
+     * 用户年龄
+     */
+    @ApiModelProperty(value = "用户年龄")
     private Integer age;
 
 }
--- src/main/java/com/xueyou/study/serviceApi/service/UserService.java
+++ src/main/java/com/xueyou/study/serviceApi/service/UserService.java
@@ -1,6 +1,10 @@
 package com.xueyou.study.serviceApi.service;
 
 import com.xueyou.study.serviceApi.models.dto.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -8,16 +12,28 @@ import java.util.Map;
 /**
  * 创建 by xueyo on 2019/8/14
  */
+@Api(value = "用户信息", tags = "user")
 @RequestMapping("/user")
 public interface UserService {
 
-    @RequestMapping(value = "/hello1", method = RequestMethod.GET)
+    @ApiOperation(value = "测试1", notes = "param传入name", httpMethod = "GET", produces = "application/json")
+    @GetMapping(value = "/hello1")
     String hello(@RequestParam("name") String name);
 
-    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
+    @ApiOperation(value = "测试2", notes = "head传入name,age", httpMethod = "HEAD")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "姓名", required = true),
+            @ApiImplicitParam(name = "age", value = "年龄", required = true)
+    })
+    @GetMapping(value = "/hello2")
     User hello(@RequestHeader("name") String name, @RequestHeader("age") Integer age);
 
-    @RequestMapping(value = "/hello3", method = RequestMethod.POST)
+    @ApiOperation(value = "测试3", notes = "body传入user实体", httpMethod = "POST", produces = "application/json")
+    @PostMapping(value = "/hello3")
     String hello(@RequestBody User user);
+
+    @ApiOperation(value = "loadBalanced", notes = "测试负载均衡", httpMethod = "GET", produces = "application/json")
+    @GetMapping(value = "/load-balanced")
+    String loadBalanced();
 
 }
