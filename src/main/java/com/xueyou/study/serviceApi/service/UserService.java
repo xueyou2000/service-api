326e906c689a4d95a5904e90da80404c1c195a3d
XueYou <xueyoucd@gmail.com>
2019-08-16 17:37:54 +0800

    修复中文乱码

--- src/main/java/com/xueyou/study/serviceApi/service/UserService.java
+++ src/main/java/com/xueyou/study/serviceApi/service/UserService.java
@@ -16,24 +16,24 @@ import java.util.Map;
 @RequestMapping("/user")
 public interface UserService {
 
-    @ApiOperation(value = "测试1", notes = "param传入name", httpMethod = "GET", produces = "application/json")
-    @GetMapping(value = "/hello1")
+    @ApiOperation(value = "测试1", notes = "param传入name", httpMethod = "GET")
+    @GetMapping(value = "/hello1", produces = "text/plain;charset=UTF-8")
     String hello(@RequestParam("name") String name);
 
-    @ApiOperation(value = "测试2", notes = "head传入name,age", httpMethod = "HEAD")
+    @ApiOperation(value = "测试2", notes = "head传入name,age", httpMethod = "GET")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "name", value = "姓名", required = true),
             @ApiImplicitParam(name = "age", value = "年龄", required = true)
     })
-    @GetMapping(value = "/hello2")
+    @RequestMapping(value = "/hello2", produces = "application/json;charset=UTF-8")
     User hello(@RequestHeader("name") String name, @RequestHeader("age") Integer age);
 
-    @ApiOperation(value = "测试3", notes = "body传入user实体", httpMethod = "POST", produces = "application/json")
-    @PostMapping(value = "/hello3")
+    @ApiOperation(value = "测试3", notes = "body传入user实体", httpMethod = "POST")
+    @PostMapping(value = "/hello3", produces = "text/plain;charset=UTF-8")
     String hello(@RequestBody User user);
 
-    @ApiOperation(value = "loadBalanced", notes = "测试负载均衡", httpMethod = "GET", produces = "application/json")
-    @GetMapping(value = "/load-balanced")
+    @ApiOperation(value = "loadBalanced", notes = "测试负载均衡", httpMethod = "GET")
+    @GetMapping(value = "/load-balanced", produces = "text/plain;charset=UTF-8")
     String loadBalanced();
 
 }
